55d95cea9cf6efb59d7ce1490c309b564f02cd23
averak <abe12@mccc.jp>
2021-06-02 16:12:27 +0900

    [feat] main.pyが完成

--- main.py
+++ main.py
@@ -3,4 +3,15 @@
 from core.service.search_result_service import SearchResultService
 
 search_result_service = SearchResultService()
-search_result_service.fetch_search_results("site;医師監修")
+
+
+keywords = [
+    "医師監修",
+    "医師執筆",
+    "記事監修",
+]
+
+for keyword in keywords:
+    print(keyword)
+    search_results = search_result_service.fetch_search_results(f"site;{keyword}")
+    search_result_service.export_search_results(search_results, f"data/{keyword}.csv")
